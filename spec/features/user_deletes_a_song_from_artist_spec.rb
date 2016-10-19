9ee7529635dfb6946623f33dd593cbaaaeabd02c
Daniel Shin <shin.daniel.d@gmail.com>
2016-10-19 14:04:48 -0600

    iter 3 additional

--- spec/features/user_deletes_a_song_from_artist_spec.rb
+++ spec/features/user_deletes_a_song_from_artist_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.feature "User deletes a song" do
   scenario "they delete from an artist's song index" do
     artist = create(:artist)
-    song = create(:song)
+    song_title = "Test"
+    artist.songs.create(title: song_title)
 
     visit artist_songs_path(artist)
     click_on "Delete"
 
     expect(page).to have_current_path (artist_songs_path(artist))
-    expect(page).not_to have_content(song.title)
+    expect(page).not_to have_content(song_title)
   end
 end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
-  factory :artist do
-    name       "Bob Marley"
+  factory :artist do |f|
+    sequence(:name) { |n| "Class #{n}" }
     image_path "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
   end
 
-
-  factory :song do
-    title "What am I Doing?"
+  factory :song do |f|
+    sequence(:title) { |n| "Class #{n}" }
     artist
   end
+
 end
